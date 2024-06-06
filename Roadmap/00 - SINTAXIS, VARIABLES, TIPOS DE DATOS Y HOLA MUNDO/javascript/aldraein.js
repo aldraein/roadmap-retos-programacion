a8c8d4389e57738fb47fe7ed1a6abc2c03aa3cfa
AcamposR <alejoext18@gmail.com>
2024-06-05 22:41:25 -0500

    Solución ejercicio 00-Sintaxis,Variables,Tipos de Datos

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/aldraein.js
@@ -0,0 +1,35 @@
+// Web oficial del lenguaje elegido : https://developer.mozilla.org/es/docs/Web/JavaScript
+// Web del estandar de JavaScript: https://262.ecma-international.org/
+
+// Con dos slash (//) se crea el comentario de una sola linea
+/* De esta manera se crea
+el comentario multiliena */
+
+//Variable
+let primerVariable;
+
+//Constante
+const miPrimeraConstante = ('');
+
+//Numeros
+let numeros = 8;
+
+//Numeros tipo float
+
+let num = 0.7
+
+//Cadenas de texto
+let String = ('Soy una cadena de texto');
+
+//Booleano
+let Booleano = true;
+
+//Valor nulo
+let valor1 = null
+//Valor indefinido
+let valor = undefined;
+
+let miLenguaje = 'JavaScript' 
+console.log ('Hola, ${miLenguaje}.');
+
+
